3fc20d22a08201458c8c848cdcc5af5171212ef1
alex <alkmas@mail.ru>
2016-10-15 23:36:59 +0300

    update model

--- db/aviacompany_postgres_create.sql
+++ db/aviacompany_postgres_create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE "empoyee" (
 	"id" serial NOT NULL,
 	"name" character varying(256) NOT NULL,
-	"state_id" bigint NOT NULL,
 	"company_id" bigint NOT NULL,
 	"job_title_id" bigint NOT NULL,
+	"state_id" bigint NOT NULL,
 	CONSTRAINT empoyee_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
@@ -34,7 +34,6 @@ CREATE TABLE "company" (
 CREATE TABLE "job_title" (
 	"id" serial NOT NULL,
 	"name" character varying(256) NOT NULL UNIQUE,
-	"divison" bigint NOT NULL,
 	CONSTRAINT job_title_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
@@ -45,7 +44,7 @@ CREATE TABLE "job_title" (
 CREATE TABLE "flight" (
 	"id" serial NOT NULL,
 	"name" character varying(256) NOT NULL,
-	"flight_id" bigint NOT NULL,
+	"team_id" bigint NOT NULL,
 	"arrival" DATE,
 	CONSTRAINT flight_pk PRIMARY KEY ("id")
 ) WITH (
@@ -56,7 +55,7 @@ CREATE TABLE "flight" (
 
 CREATE TABLE "team" (
 	"id" serial NOT NULL,
-	"number" bigint NOT NULL,
+	"number" character varying(256) NOT NULL,
 	"count_pilot" bigint NOT NULL,
 	"count_navigator" bigint NOT NULL,
 	"count_radioman" bigint NOT NULL,
@@ -77,15 +76,14 @@ CREATE TABLE "team_2_employee" (
 
 
 
-ALTER TABLE "empoyee" ADD CONSTRAINT "empoyee_fk0" FOREIGN KEY ("state_id") REFERENCES "state"("id");
-ALTER TABLE "empoyee" ADD CONSTRAINT "empoyee_fk1" FOREIGN KEY ("company_id") REFERENCES "company"("id");
-ALTER TABLE "empoyee" ADD CONSTRAINT "empoyee_fk2" FOREIGN KEY ("job_title_id") REFERENCES "job_title"("id");
+ALTER TABLE "empoyee" ADD CONSTRAINT "empoyee_fk0" FOREIGN KEY ("company_id") REFERENCES "company"("id");
+ALTER TABLE "empoyee" ADD CONSTRAINT "empoyee_fk1" FOREIGN KEY ("job_title_id") REFERENCES "job_title"("id");
+ALTER TABLE "empoyee" ADD CONSTRAINT "empoyee_fk2" FOREIGN KEY ("state_id") REFERENCES "state"("id");
 
 
 
-ALTER TABLE "job_title" ADD CONSTRAINT "job_title_fk0" FOREIGN KEY ("divison") REFERENCES "division"("id");
 
-ALTER TABLE "flight" ADD CONSTRAINT "flight_fk0" FOREIGN KEY ("flight_id") REFERENCES "team"("id");
+ALTER TABLE "flight" ADD CONSTRAINT "flight_fk0" FOREIGN KEY ("team_id") REFERENCES "team"("id");
 
 
 ALTER TABLE "team_2_employee" ADD CONSTRAINT "team_2_employee_fk0" FOREIGN KEY ("team_id") REFERENCES "team"("id");
