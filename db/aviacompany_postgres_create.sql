4105307ccb8f8ecbad846ea11f30409921e80ee9
alex <askmefile@gmail.com>
2016-10-19 14:22:21 +0300

    no comments

--- db/aviacompany_postgres_create.sql
+++ db/aviacompany_postgres_create.sql
@@ -1,30 +1,8 @@
 CREATE TABLE "empoyee" (
 	"id" serial NOT NULL,
 	"name" character varying(256) NOT NULL,
-	"company_id" bigint NOT NULL,
 	"job_title_id" bigint NOT NULL,
-	"state_id" bigint NOT NULL DEFAULT '1',
 	CONSTRAINT empoyee_pk PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-CREATE TABLE "state" (
-	"id" serial NOT NULL,
-	"name" character(8) NOT NULL UNIQUE,
-	CONSTRAINT state_pk PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-CREATE TABLE "company" (
-	"id" serial NOT NULL,
-	"name" character varying(256) NOT NULL UNIQUE,
-	CONSTRAINT company_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -43,8 +21,13 @@ CREATE TABLE "job_title" (
 
 CREATE TABLE "flight" (
 	"id" serial NOT NULL,
-	"name" character varying(256) NOT NULL,
-	"flying_away" DATE,
+	"name" character varying(20) NOT NULL,
+	"way" character varying(256) NOT NULL,
+	"away_date" DATE NOT NULL,
+	"away_time" TIME NOT NULL,
+	"arrival_date" DATE NOT NULL,
+	"arrival_time" TIME NOT NULL,
+	"terminal" character,
 	CONSTRAINT flight_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
@@ -54,7 +37,7 @@ CREATE TABLE "flight" (
 
 CREATE TABLE "team" (
 	"id" serial NOT NULL,
-	"number" character varying(256) NOT NULL,
+	"name" character varying(256) NOT NULL,
 	"count_pilot" bigint NOT NULL,
 	"count_navigator" bigint NOT NULL,
 	"count_radioman" bigint NOT NULL,
@@ -84,11 +67,33 @@ CREATE TABLE "team_2_flight" (
 
 
 
-ALTER TABLE "empoyee" ADD CONSTRAINT "empoyee_fk0" FOREIGN KEY ("company_id") REFERENCES "company"("id");
-ALTER TABLE "empoyee" ADD CONSTRAINT "empoyee_fk1" FOREIGN KEY ("job_title_id") REFERENCES "job_title"("id");
-ALTER TABLE "empoyee" ADD CONSTRAINT "empoyee_fk2" FOREIGN KEY ("state_id") REFERENCES "state"("id");
+CREATE TABLE "state" (
+	"id" serial NOT NULL,
+	"name" character varying(256) NOT NULL UNIQUE,
+	"need_date_time" BOOLEAN NOT NULL DEFAULT 'false',
+	CONSTRAINT state_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
 
 
+
+CREATE TABLE "flight_2_state" (
+	"id" serial NOT NULL,
+	"flight_id" bigint NOT NULL,
+	"state_id" bigint NOT NULL,
+	"info_date" DATE,
+	"info_time" TIME,
+	"create_date" DATE NOT NULL,
+	"create_time" TIME NOT NULL,
+	CONSTRAINT flight_2_state_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+ALTER TABLE "empoyee" ADD CONSTRAINT "empoyee_fk0" FOREIGN KEY ("job_title_id") REFERENCES "job_title"("id");
 
 
 
@@ -98,4 +103,8 @@ ALTER TABLE "team_2_employee" ADD CONSTRAINT "team_2_employee_fk1" FOREIGN KEY (
 
 ALTER TABLE "team_2_flight" ADD CONSTRAINT "team_2_flight_fk0" FOREIGN KEY ("team_id") REFERENCES "team"("id");
 ALTER TABLE "team_2_flight" ADD CONSTRAINT "team_2_flight_fk1" FOREIGN KEY ("flight_id") REFERENCES "flight"("id");
+
+
+ALTER TABLE "flight_2_state" ADD CONSTRAINT "flight_2_state_fk0" FOREIGN KEY ("flight_id") REFERENCES "flight"("id");
+ALTER TABLE "flight_2_state" ADD CONSTRAINT "flight_2_state_fk1" FOREIGN KEY ("state_id") REFERENCES "state"("id");
 
